346bb9bf5655fcd0a2691d07dc678031f7558d33
unknown <bbb155072@gmail.com>
2024-11-30 00:03:20 +0300

    first commit

---
+++ RMC-main/js.js
@@ -0,0 +1,15 @@
+// let arrowLeft = document.querySelector('.arrow.left');
+// let arrowRight = document.querySelector('.arrow.right');
+// let limitation = document.querySelector('.limitation');
+
+
+// limitation.style.scrollBehavior = 'smooth'; 
+// arrowLeft.addEventListener('click', ()=>{
+//   console.log('left');
+//   limitation.scrollLeft -= 516;
+// });
+
+// arrowRight.addEventListener('click', ()=>{
+//   console.log('right');
+//   limitation.scrollLeft += 516;
+// });
